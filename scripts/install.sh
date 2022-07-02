88d65fae86897f59ccc9f1aab20dc5074f878292
Carlos Castillo <47466248+carlosecp@users.noreply.github.com>
2022-07-02 14:30:40 -0300

    reverting removed changes, adding xinput controls to xinitrc

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,46 +1,32 @@
-#!/usr/bin/env bash
+#!/usr/bin/env -S bash -x
 
-install_command="paru -S --needed --noconfirm --quiet"
-
-# dependencies
-sudo pacman -S rustup --needed --noconfirm
-rustup install stable
-rustup default stable
+install_command="paru -S --needed --noconfirm --noredownload"
 
 if ! command -v paru > /dev/null; then
+	sudo pacman -S rustup --needed --noconfirm
+	rustup install stable
+	rustup default stable
+
 	build_dir=`mktemp -d`
 	git clone https://aur.archlinux.org/paru.git $build_dir
 	cd $build_dir && makepkg -si --noconfirm
 fi
 
-# base packages (arch repos)
 packages=(
 	acpi
 	awesome
-	flameshot 
+	dragon-drop
+	maim
 	kvantum
 	lxappearance
 	noto-fonts
+	nsxiv
 	pcmanfm-gtk3
 	playerctl 
 	spotifywm
 	xbindkeys 
+	xclip-git
 	xorg-xbacklight 
 )
 
 $install_command ${packages[@]}
-
-# aur pacakges
-# format: ["binary"]="package"
-declare -A aur_packages
-aur_packages=(
-	["dragon-drop"]="dragon-drop"
-	["nsxiv"]="nsxiv"
-	["xclip"]="xclip-git"
-)
-
-for package in "${!aur_packages[@]}"; do
-	if ! command -v $package > /dev/null; then
-		$install_command ${aur_packages[$package]}
-	fi
-done
